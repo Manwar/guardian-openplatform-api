129112e018407c6ca079900fe4cf121f98f5d01c
Dave Cross <dave@dave.org.uk>
2009-03-11 20:09:52 +0000

    Tests for basic object construction, together with code that passes them.

--- lib/Guardian/OpenPlatform/API.pm
+++ lib/Guardian/OpenPlatform/API.pm
@@ -1,9 +1,36 @@
 package Guardian::OpenPlatform::API;
 
 use 5.006000;
-use strict;
-use warnings;
+
+use Moose;
+use LWP;
+use Carp;
 
 our $VERSION = '0.01';
+
+my $base_url = 'http://api.gusrdianapis.com/';
+
+has 'ua' => (
+    is => 'rw',
+    isa => 'LWP::UserAgent',
+    );
+
+has 'api_key' => (
+    is => 'rw',
+    isa => 'Str',
+    required => 1,
+    );
+
+has 'format' => (
+    is => 'rw',
+    isa => 'Str',
+    default => 'json',
+    );
+
+sub BUILD {
+    my $self = shift;
+
+    $self->{ua} = LWP::UserAgent->new;
+}
 
 1;
--- t/00-load.t
+++ t/00-load.t
@@ -1,3 +1,12 @@
-use Test::More tests => 1;
+use Test::More 'no_plan';
+
 BEGIN { use_ok('Guardian::OpenPlatform::API') };
 
+ok(my $client = Guardian::OpenPlatform::API->new(
+  api_key => $ENV{GUARDIAN_API_KEY},
+), 'Got client');
+isa_ok($client, 'Guardian::OpenPlatform::API');
+
+is($client->format, 'json', 'Default format correct');
+isa_ok($client->ua, 'LWP::UserAgent');
+is($client->api_key, $ENV{GUARDIAN_API_KEY}, 'API key correct');
